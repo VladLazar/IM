3bfdadd6c2670ba4158c18c2b389e453524e4c7f
balu <vlalazar.vlad@gmail.com>
2016-07-18 14:24:31 +0300

    Commented the code... Sort of

--- client/client.py
+++ client/client.py
@@ -1,7 +1,7 @@
 # client
 
 import sys, socket, select
- 
+
 def chat_client():
     if(len(sys.argv) < 3) :
         print 'Usage : python client.py <hostname> <port>'
@@ -9,25 +9,27 @@ def chat_client():
 
     host = sys.argv[1]
     port = int(sys.argv[2])
-     
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(2)
-     
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create socket
+    s.settimeout(10)
+
     try :
-        s.connect((host, port))
+        s.connect((host, port)) # connect socket to server
     except :
         print 'Unable to connect'
         sys.exit()
-     
+
     print 'Connected to remote host. You can start sending messages'
     sys.stdout.write('[Me] '); sys.stdout.flush()
-     
+
     while 1:
         socket_list = [sys.stdin, s]
-         
+
+        # select.select method is a straight interface to the UNIX System
+        # Connections are added and removed from these lists by the server main loop
         read_sockets, write_sockets, error_sockets = select.select(socket_list , [], [])
-         
-        for sock in read_sockets:            
+
+        for sock in read_sockets:
             if sock == s:
                 data = sock.recv(4096)
                 if not data :
@@ -35,12 +37,11 @@ def chat_client():
                     sys.exit()
                 else :
                     sys.stdout.write(data)
-                    sys.stdout.write('[Me] '); sys.stdout.flush()     
-            
+                    sys.stdout.write('[Me] '); sys.stdout.flush()
             else :
                 msg = sys.stdin.readline()
                 s.send(msg)
-                sys.stdout.write('[Me] '); sys.stdout.flush() 
+                sys.stdout.write('[Me] '); sys.stdout.flush()
 
 if __name__ == "__main__":
 
