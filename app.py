9e487b45d66dcc6c00b02b33dcc4d4595f550980
VladLazar <vlalazar.vlad@gmail.com>
2016-08-03 20:34:43 +0300

    Created Authentification with token and hashed passwords

--- app.py
+++ app.py
@@ -1,53 +1,25 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
 from flask import Flask, session, request, flash, url_for, redirect, render_template, abort, g
-from flask.ext.login import login_user, logout_user, current_user, login_required, LoginManager
+from flask_login import login_user, logout_user, current_user, login_required, LoginManager
 from flask_sqlalchemy import SQLAlchemy
+import os
+from forms import LoginForm, RegisterForm
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SECRET_KEY'] = 'super-secret'
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://balu:Baweelia1@localhost/yournewdb"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-
-class User(db.Model):
-
-    __tablename__ = 'user'
-
-    id = db.Column('user_id', db.Integer, primary_key=True)
-    username = db.Column('username', db.String(20), unique=True, index=True)
-    password = db.Column('password', db.String(10))
-    email = db.Column('email', db.String(50), unique=True, index=True)
-    online = db.Column('online', db.Boolean)
-
-    def __init__(self, username, password, email, online):
-        self.username = username
-        self.password = password
-        self.email = email
-        self.online = online
-
-    def is_active(self):
-        """All users are active"""
-        return True
-
-    def get_id(self):
-        return str(self.id)
-
-    def is_authenticated(self):
-        """Return True if user is authenticated"""
-        return self.authenticated
-
-    def is_anonymous(self):
-        """False, anonymous users not supported"""
-        return False
-
-    def __repr__(self):
-        return '<User %r>' % (self.username)
-
-
+bcrypt = Bcrypt()
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
+
+from models import User, db
 
 
 @app.route('/')
@@ -57,43 +29,54 @@ def index():
 
 
 @login_manager.user_loader
-def load_user(id):
-    return User.query.get(int(id))
+def user_loader(user_id):
+    return User.query.get(user_id)
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    form = RegisterForm()
     if request.method == 'GET':
-        return render_template('register.html')
-    user = User(request.form['username'], request.form['password'], request.form['email'])
-    db.session.add(user)
-    db.session.commit()
-    flash('User succesfully registered')
-    return redirect(url_for('login'))
+        return render_template('register.html', form=form)
+    if form.validate_on_submit():
+        username = form.username.data
+        password = bcrypt.generate_password_hash(form.password.data)
+        email = form.email.data
+        user = User(username, password, email, False)
+        db.session.add(user)
+        db.session.commit()
+        flash('User successfully registered')
+        return redirect(url_for('login'))
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    form = LoginForm()
     if request.method == 'GET':
-        return render_template('login.html')
-    username = request.form['username']
-    password = request.form['password']
-    registered_user = User.query.filter_by(username=username, password=password).first()
-    if registered_user is None:
-        flash('Username or password is invalid!')
-        return redirect(url_for('login'))
-    login_user(registered_user)
-    flash('Logged in successfully!')
-    registered_user.online = True
-    return redirect(url_for('index'))
+        return render_template('login.html', form=form)
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                user.authenticated = True
+                db.session.add(user)
+                db.session.commit()
+                login_user(user)
+                flash('Logged in successfully!')
+                return redirect(url_for('index'))
+    return render_template('login.html', form=form)
+
 
 @app.before_request
 def before_request():
     g.user = current_user
 
+
 @app.route('/logout')
 def logout():
-    g.user.online = False
+    user = current_user
+    user.authenticated = False
+    db.session.add(user)
+    db.session.commit()
     logout_user()
     return render_template('logout.html')
-
