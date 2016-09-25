57067b4a174aef4220e15bcd041a99ee1f03e2d6
VladLazar <vlalazar.vlad@gmail.com>
2016-09-25 15:22:55 +0300

    Msg

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ from forms import LoginForm, RegisterForm
 from flask import json
 import jsonpickle
 
-
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config.from_object(os.environ['APP_SETTINGS'])
@@ -22,14 +21,20 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
-from models import User, Conversation, Message, db
+from models import User, Conversation, Message, Invite, db
 
 
 @app.route('/')
 @login_required
 def index():
     authenticated_users = User.query.filter_by(authenticated=True).all()
-    return render_template('index.html', authenticated_users=authenticated_users, current_username=current_user.username)
+    invites_of_current_user = current_user.invites
+    conferences_of_user = Conversation.query.filter(
+                        Conversation.users.contains(current_user),
+                        Conversation.is_conference == True
+                        )
+    return render_template('index.html', authenticated_users=authenticated_users, current_username=current_user.username
+                           ,invites=invites_of_current_user, conferences=conferences_of_user)
 
 
 @login_manager.user_loader
@@ -43,7 +48,7 @@ def register_get():
     return render_template('register.html', form=form)
 
 
-@app.route('/register', methods=['POST'])
+@app.route('/register', methods=['GET', 'POST'])
 def register_post():
     form = RegisterForm()
     if form.validate_on_submit():
@@ -54,31 +59,27 @@ def register_post():
         db.session.add(user)
         db.session.commit()
         flash('User successfully registered')
-        return redirect(url_for('login_get'))
+        return redirect(url_for('login'))
     return render_template('register.html', form=form)
 
 
-@app.route('/login', methods=['GET'])
-def login_get():
+@app.route('/login', methods=['GET', 'POST'])
+def login():
     form = LoginForm()
-    if request.method == 'GET':
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            user = User.query.filter_by(username=form.username.data).first()
+            if user:
+                if bcrypt.check_password_hash(user.password, form.password.data):
+                    user.authenticated = True
+                    db.session.add(user)
+                    db.session.commit()
+                    login_user(user)
+                    flash('Logged in successfully!')
+                    return redirect(url_for('index'))
         return render_template('login.html', form=form)
-
-
-@app.route('/login', methods=['POST'])
-def login_post():
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user:
-            if bcrypt.check_password_hash(user.password, form.password.data):
-                user.authenticated = True
-                db.session.add(user)
-                db.session.commit()
-                login_user(user)
-                flash('Logged in successfully!')
-                return redirect(url_for('index'))
-    return render_template('login.html', form=form)
+    elif request.method == 'GET':
+        return render_template('login.html', form=form)
 
 
 @app.route('/conversation/<partner_username>')
@@ -91,17 +92,23 @@ def conversation(partner_username):
             # If the users are alone in the conversation
             if (current_user in users_in_current) and (partner_user in users_in_current) and len(users_in_current) <= 2:
                 return render_template('chat.html', conversation_id=current.id,
-                                       username=current_user.username, other_user=partner_username)
+                                       username=current_user.username)
     print partner_user
     print current_user
-    new_convo = Conversation()
+    new_convo = Conversation(is_conference=False)
     new_convo.users.append(current_user)
     new_convo.users.append(partner_user)
     db.session.add(new_convo)
     db.session.commit()
     return render_template('chat.html', conversation_id=new_convo.id,
-                           username=current_user.username, other_user=partner_username)
+                           username=current_user.username)
 
+
+@app.route('/conference/<conference_id>')
+def conference(conference_id):
+    current_conference = Conversation.query.filter_by(id=conference_id).first()
+    return render_template('chat.html', conversation_id=conference_id,
+                           username=current_user.username)
 
 @app.route('/api/get_users', methods=['GET'])
 def get_users():
@@ -113,7 +120,7 @@ def get_users():
 def get_and_post_conversation(conversation_id):
     current_conversation = Conversation.query.filter_by(id=conversation_id).first()
     if request.method == 'POST':
-        new_message = Message(request.json['message'], request.json['timestamp'], request.json['sender'])
+        new_message = Message(message=request.json['message'], timestamp=request.json['timestamp'], sender=request.json['sender'])
         current_conversation.messages.append(new_message)
         db.session.add(new_message)
         db.session.commit()
@@ -124,6 +131,48 @@ def get_and_post_conversation(conversation_id):
         return jsonpickle.encode(passed_list)
 
 
+@app.route('/create_conference', methods=['GET'])
+def create_conference():
+    users = User.query.all()
+    new_convo = Conversation(is_conference=True)
+    db.session.add(new_convo)
+    db.session.commit()
+    new_convo.users.append(current_user)
+    new_convo.messages.append(Message(message='New conference created', timestamp='4:20', sender='Server'))
+    new_invite = Invite(initiator=current_user.username, conference_id=new_convo.id)
+    db.session.add(new_invite)
+    db.session.commit()
+    return render_template('create_conference.html', invite=new_invite, users=users)
+
+
+@app.route('/api/send_invite/<username_to_query>', methods=['POST'])
+def send_invite(username_to_query):
+    invitee = User.query.filter_by(username=username_to_query).first()
+    invite = Invite(initiator=request.json["initiator"], conference_id=request.json["conference_id"])
+    invitee.invites.append(invite)
+    db.session.add(invite)
+    db.session.commit()
+    return '<h1>Invite sent</h1>'
+
+
+@app.route('/api/accept_invite/<cur_conf_id>', methods=['POST'])
+def accept_invite(cur_conf_id):
+    cur_conf = Conversation.query.filter_by(id=cur_conf_id).first()
+    cur_conf.users.append(current_user)
+    cur_conf.messages.append(Message(message='User ' + current_user.username + ' joined!', timestamp='4:20',
+                                     sender='Server'))
+    
+    invite_id = request.json["invite_id"]
+    invite = Invite.query.filter_by(id=invite_id).delete()
+
+    db.session.commit()
+    return '<h1>Invite accepted</h1>'
+
+"""
+@app.route('')
+def user_page():
+"""
+
 @app.before_request
 def before_request():
     g.user = current_user
@@ -133,7 +182,6 @@ def before_request():
 def logout():
     user = current_user
     user.authenticated = False
-    db.session.add(user)
     db.session.commit()
     logout_user()
     return render_template('logout.html')
--- models.py
+++ models.py
@@ -16,6 +16,7 @@ class User(db.Model, UserMixin):
     password = db.Column('password', db.String(100))
     email = db.Column('email', db.String(100), unique=True, index=True)
     authenticated = db.Column('authenticated', db.Boolean, default=False)
+    invites = db.relationship("Invite", backref="user", lazy="dynamic")
 
     def __init__(self, username, password, email, authenticated):
         self.username = username
@@ -39,6 +40,7 @@ class Conversation(db.Model):
     id = db.Column('id', db.Integer, primary_key=True)
     users = db.relationship("User", secondary=relationship_table)
     messages = db.relationship("Message", backref="conversation", lazy="dynamic")
+    is_conference = db.Column('is_conference', db.Boolean)
 
 
 class Message(db.Model):
@@ -50,12 +52,21 @@ class Message(db.Model):
     sender = db.Column('sender', db.String)
     conversation_id = db.Column(db.Integer, db.ForeignKey('conversation.id'))
 
-    def __init__(self, message, timestamp, sender):
-        self.message = message
-        self.timestamp = timestamp
-        self.sender = sender
-
     def __repr__(self):
         return '<%r: %r>' % (self.sender, self.message)
+
+
+class Invite(db.Model):
+    __tablename__ = 'invite'
+
+    id = db.Column('id', db.Integer, primary_key=True)
+    initiator = db.Column('initiator', db.String)
+    conference_id = db.Column('conference_id', db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __init__(self, initiator, conference_id):
+        self.initiator = initiator
+        self.conference_id = conference_id
+
 
 
